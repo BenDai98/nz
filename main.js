555e3da8b7cbbc4ee0b1a569055224100983a5ad
Benjamin Daiß <benjamin.daiss@student.uibk.ac.at>
2024-04-11 15:56:16 +0200

    var to let

--- main.js
+++ main.js
@@ -1,18 +1,21 @@
-var map = L.map('map').setView([-43.595136, 170.142103], 13);
+let map = L.map('map').setView([-43.595136, 170.142103], 13);
 
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 19,
-        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    }).addTo(map);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
 
-    var marker = L.marker([-43.595136, 170.142103]).addTo(map);
-    marker.bindPopup("<strong>Hello world!</strong><br>I am a popup.").openPopup();
+let marker = L.marker([-43.595136, 170.142103]).addTo(map);
+marker.bindPopup("<strong>Hello world!</strong><br>I am a popup.").openPopup();
 
-    var circle = L.circle([-43.585754, 170.179254
+let circle = L.circle([-43.585754, 170.179254], {
+    color: 'red',
+    fillColor: '#f03',
+    fillOpacity: 0.5,
+    radius: 500
+}).addTo(map);
 
-    ], {
-        color: 'red',
-        fillColor: '#f03',
-        fillOpacity: 0.5,
-        radius: 500
-    }).addTo(map);
+console.log(`
+    <h3>Test für "ABC"</h3>
+    another text
+`);
